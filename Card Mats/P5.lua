615a54d5a704d446c3a3dff3d93958b83dfa0987
Unebrion <tristansmith8614@gmail.com>
2024-03-17 13:09:45 -0400

    starting version control

---
+++ Card Mats/P5.lua
@@ -0,0 +1,65 @@
+function onload()
+
+  self.createButton({ index = 0, label="Draw 1", click_function="Comprar1", function_owner=self,
+  position={0,1,-11}, rotation={0,0,0}, height=1200, width=2500, font_size = 500, font_color={0,0,0}   })
+
+end
+
+function Comprar1(objectButtonClicked, playerColorClicked)
+
+N_jg = 5
+Tab_Nomes = { "b5397c", "63933c", "fd704b", "869398", "6943aa", "6dd41f" }
+Cor_Jog = getObjectFromGUID(Tab_Nomes[N_jg]).getColorTint():toString()
+
+if playerColorClicked == Cor_Jog then
+
+printToAll("The player ".. Player[Cor_Jog].steam_name .. " drew one card!", Cor_Jog)
+
+Zona_Baralho_guid  = {"0fc05c", "b0259a", "4b8cc5", "698e9c", "2a3183", "72b34d",  } getObjectFromGUID("0fc05c")
+Zona_Descarte_guid = {"646d6c", "24bdbc", "18841d", "71d6da", "fffe8d", "7b00f5",  } getObjectFromGUID("646d6c")
+
+Zona_Baralho  = getObjectFromGUID(Zona_Baralho_guid[N_jg])
+Zona_Descarte = getObjectFromGUID(Zona_Descarte_guid[N_jg])
+
+for i=1, 1, 1 do
+  --compra 1 carta
+local cartas = Zona_Baralho.getObjects()
+  if #cartas > 1 then --compra 1 carta
+    for j=1, #cartas, 1 do
+    local obj = cartas[j]
+    if(obj.tag == "Deck") then
+    obj.deal(1, Cor_Jog)
+  elseif (obj.tag == "Card") then
+      obj.deal(1, Cor_Jog)
+    end
+    end
+else
+  local cartas = Zona_Descarte.getObjects()
+    if #cartas > 1 then --compra 1 carta
+      for j=1, #cartas, 1 do
+      local obj = cartas[j]
+      if(obj.tag == "Deck") then
+      obj.shuffle()
+      obj.flip()
+      local pos = obj.getPosition()
+      obj.setPositionSmooth({pos["x"]-4.4, pos["y"]+2, pos["z"]})
+      obj.deal(1, Cor_Jog)
+
+      --obj.setPositionSmooth(Table, bool, bool)
+
+      end
+      end
+    end
+
+
+
+  end -- if
+
+
+end -- for
+else
+broadcastToAll("Somente o jogador " .. Player[Cor_Jog].steam_name .. " pode pegar cartas deste baralho!", {0,0,0})
+end
+
+end --func
+--------------------------
---
+++ main.lua
@@ -0,0 +1,89 @@
+
+function hideWelcome()
+  UI.hide("welcome")
+end
+
+function onLoad()
+
+
+getObjectFromGUID("b5fb7e").interactable = false -- chão
+
+--getObjectFromGUID("a26aa7").setPosition({-15.90, 1.53, -7.70}) -- botão começar
+
+end
+
+
+function onClick(objectButtonClicked, playerColorClicked)
+Cor_Jogador_Clicou = playerColorClicked
+print(Cor_Jogador_Clicou)
+end
+
+
+-- Blockade Callbacks --
+
+x_bar = 0
+function takeBlockade(object, playerColorClicked)
+B_guid = object.getGUID() -- da378b
+  
+barreira = getObjectFromGUID(B_guid)
+players = playerColorClicked
+
+giveObjectToPlayer(barreira,players, 0.65)
+
+x_bar = x_bar +0.6
+
+barreira.removeButton(0)
+end
+
+function giveObjectToPlayer(object, player, extraDistance)
+--local hand = Player[playerColour]:getPlayerHand()
+local playerHand = Player[player].getPlayerHand();
+
+  object.setRotationSmooth({0,playerHand["rot_y"]+0,0})
+  object.setPositionSmooth({playerHand["pos_x"]+ 4.5 + x_bar +playerHand["trigger_forward_x"]*15,   1.5,   playerHand["pos_z"]-1.3+(playerHand["trigger_forward_z"]*extraDistance)*15})
+end
+
+function Chama_Baixar()
+--  print(playerColorClicked)
+
+print(Cor_Jogador_Clicou)
+
+--  jogador = Player[playerColorClicked]
+
+--print(jogador)
+
+
+
+Tab_Principal = getObjectFromGUID("8008a7")
+Tab_Principal.call("Baixar",objectButtonClicked, playerColorClicked )
+end
+
+function Baixar2()
+print("ok - Global")
+end
+
+
+
+function onScriptingButtonDown(index, player_color)
+  if index == 1 then
+      local bag = getObjectFromGUID("02a357")
+      if bag then 
+          setNameToItemsInBag(bag)
+      else
+          print("Bag not found")
+      end
+  end
+
+  if index == 2 then 
+    local sourceBag = getObjectFromGUID("02a357")
+    local objPos = sourceBag.getPosition() + Vector (0,2,0)
+    for _, tile in ipairs(sourceBag.getObjects()) do 
+      local tileObj = sourceBag.takeObject({position = objPos})
+      tileObj.setName("Cave")
+    end
+  end
+
+  if index == 3 then 
+    print("andy sucks .com")
+  end
+end
